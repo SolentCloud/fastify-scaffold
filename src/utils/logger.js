f8a4fe236ce69b4d30912fb23274c1dc005a0a11
DraytonCoding <nick@draytoncoding.com>
2022-02-27 14:29:52 +0000

    feat(logger): elapsed time now displays in accurate mills

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,24 +1,36 @@
-const now = () => Date.now();
+import prettyHrTime from "pretty-hrtime";
 
 const configureLogging = async (server) => {
   // setup request logging
-  server.addHook('onRequest', (req, reply, done) => {
-    reply.startTime = now();
-    req.log.info({
-      url: req.raw.url,
-    }, 'received request');
+  server.addHook("onRequest", (req, reply, done) => {
+    reply.startTime = process.hrtime();
+
+    req.log.info(
+      {
+        url: req.raw.url,
+      },
+      "received request"
+    );
     done();
   });
 
   // setup response response
-  server.addHook('onResponse', (req, reply, done) => {
-    req.log.info({
-      url: req.raw.url,
-      statusCode: reply.raw.statusCode,
-      durationMs: now() - reply.startTime,
-    }, 'request completed');
+  server.addHook("onResponse", (req, reply, done) => {
+    const diff = process.hrtime(reply.startTime);
+
+    req.log.info(
+      {
+        url: req.raw.url,
+        statusCode: reply.raw.statusCode,
+        elapsedMs: hrtimeToMs(diff),
+        elapsedPretty: prettyHrTime(diff, { precise: true }),
+      },
+      "request completed"
+    );
     done();
   });
 };
+
+export const hrtimeToMs = (diff = []) => diff[0] * 1e3 + diff[1] * 1e-6;
 
 export default configureLogging;
