ba2df9c78e87c34e650d8eaba46a3149146e3fd9
nick@draytoncoding.com <nick@draytoncoding.com>
2022-03-11 09:56:27 +0000

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -2,3 +2,4 @@ curl -X POST http://localhost:4001/user \
   -H 'Content-Type: application/json' \
   -H 'trace_id: 65086975-6b55-4ea3-8c8f-f100a0edce27' \
   -d '{"username": "bob123", "emailAddress": "bob@example"}'
+  
